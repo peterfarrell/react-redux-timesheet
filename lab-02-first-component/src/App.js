5cabc698610050d06cd5e0c45592410c93de7cb8
peterfarrell <peter_farrell@saa.senate.gov>
2018-08-08 09:53:31 -0500

    Extra credit

--- lab-02-first-component/src/App.js
+++ lab-02-first-component/src/App.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
+import Hello from './hello/Hello'
 
 class App extends Component {
-  render() {
-    return <div className="App" />;
+  render () {
+    return (
+      <div className='App'>
+        <Hello friend='Peter' />
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ lab-02-first-component/src/hello/Hello.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import Hello from './Hello';
+import renderer from 'react-test-renderer';
+
+describe('Hello World:', () => {
+    it('renders without exploding', () => {
+        expect(shallow(<Hello/>)).toHaveLength(1);
+    });
+
+    it('should render with default text', () => {
+        const component = shallow(<Hello/>);
+
+        expect(component).toIncludeText('Howdy');
+        expect(component).toIncludeText('Partner');
+    });
+
+    it('should render with our props', () => {
+        const component = shallow(<Hello friend="Fred"/>);
+
+        expect(component).toIncludeText('Howdy');
+        expect(component).toIncludeText('Fred');
+        expect(component).not.toIncludeText('Partner');
+    });
+
+    it('should render to match the snapshot', () => {
+      const component = renderer.create(<Hello friend="Peter" />);
+
+      expect(component.toJSON()).toMatchSnapshot();
+    });
+});
